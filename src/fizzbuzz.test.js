e411e3f2b8aa5511c788be1fa180743600a94588
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-09-24 11:12:28 +0000

    Refactoring Test

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,9 +1,10 @@
 const fizzbuzz = require("./fizzbuzz");
-const sum = require("./fizzbuzz");
 
-test("fizzbuzz", () => {
-  const result = fizzbuzz(1);
-  const expected = 1;
+describe("fizzbuzz", () => {
+  test("Shoud return 1 when receives 1", () => {
+    const result = fizzbuzz(1);
+    const expected = 1;
 
-  expect(1).toBe(1);
+    expect(1).toBe(1);
+  });
 });
